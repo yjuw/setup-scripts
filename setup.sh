639dbe2cea1af3b75a5d317a57815f7fbc32b849
yjuw <byk229@uw.edu>
2024-04-02 00:11:12 -0700

    fix: copy paste signal install

--- setup.sh
+++ setup.sh
@@ -9,11 +9,14 @@ if [[ $APT ]]; then
     apt install flatpak -y
     apt install curl wget gpupg -y
     #signal
-    wget -O https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+    # 1. Install our official public software signing key:
+    wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
     cat signal-desktop-keyring.gpg | tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+    # 2. Add our repository to your list of repositories:
     echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
       tee /etc/apt/sources.list.d/signal-xenial.list
-    apt update -y && apt install signal-desktop -y
+    # 3. Update your package database and install Signal:
+sudo apt update && sudo apt install signal-desktop
     #discord
     wget -O discord.deb https://discord.com/api/download?platform=linux&format=deb
     dpkg -i discord.deb
